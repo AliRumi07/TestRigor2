482640c086e6ad5bab057df95bdd1cbe75e6c770
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-11-30 22:53:58 +0500

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,5 @@
+curl -X POST \
+  -H 'Content-type: application/json' \
+  -H 'auth-token: 92e3793b-3471-4e4e-9a2e-2373f65f38d2' \
+  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/XzKgYjEdpJxvfk9lYe9uGMJCVfKxhy/Business_Finance_35.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  https://api.testrigor.com/api/v1/apps/5GztF8H4oWxgJ3JN3/retest
