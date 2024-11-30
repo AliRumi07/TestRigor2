2c8407423df4be7c5876c1047a5ff783fbfe74c2
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-01 00:01:45 +0500

    Delete 10.sh

--- 10.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST \
-  -H 'Content-type: application/json' \
-  -H 'auth-token: 5d110fa3-3239-4482-8b49-ef9c5b03af3c' \
-  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/XzKgYjEdpJxvfk9lYe9uGMJCVfKxhy/Business_Finance_35.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
-  https://api.testrigor.com/api/v1/apps/HNkRoMswMfPdGusYB/retest
